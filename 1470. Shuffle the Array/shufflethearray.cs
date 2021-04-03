22f7a6f7819e0492893e018f3960895659de9efe
Michael Elwood <melwood@proformex.com>
2021-04-03 14:27:40 -0400

    shuffle the array

---
+++ 1470. Shuffle the Array/shufflethearray.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public int[] Shuffle(int[] nums, int n)
+    {
+        List<int> sortedArray = new List<int>();
+
+        int yArea = n;
+        for (int x = 0; x < n; x++)
+        {
+            sortedArray.Add(nums[x]);
+            sortedArray.Add(nums[yArea]);
+            yArea++;
+        }
+
+        return sortedArray.ToArray();
+    }
+}
