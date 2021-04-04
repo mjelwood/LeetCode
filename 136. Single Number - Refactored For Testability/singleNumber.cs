47571b1717b5927db06d811e344a1d37a5a263a7
Michael Elwood <melwood@proformex.com>
2021-04-04 11:13:34 -0400

    refactored 136 for a bit more test-ability!

---
+++ 136. Single Number - Refactored For Testability/singleNumber.cs
@@ -0,0 +1,31 @@
+﻿public class Solution {
+    public int SingleNumber(int[] nums) {
+     
+        Dictionary<int, int> currentCountOfKeys = new Dictionary<int, int>();
+        int onlyAppearsOnce = 0;
+        
+        for(int i = 0; i < nums.Length; i++)
+        {
+            int curKey = nums[i];
+            if(currentCountOfKeys.ContainsKey(curKey))
+            {
+                currentCountOfKeys[curKey]++;
+            }
+            else
+            {
+                currentCountOfKeys.Add(curKey, 1);    
+            }
+        }
+        
+        foreach(KeyValuePair<int,int> item in currentCountOfKeys)
+        {
+            if(item.Value == 1)
+            {
+                onlyAppearsOnce = item.Key;
+				break; // Exit out if we found that single entry!
+            }
+        }
+        
+        return onlyAppearsOnce;
+    }
+}
