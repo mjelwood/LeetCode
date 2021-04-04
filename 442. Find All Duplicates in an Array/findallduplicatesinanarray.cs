4b65d2ae2ac55de5a04f5b66f0d338fc02ffb727
Michael Elwood <melwood@proformex.com>
2021-04-03 20:46:11 -0400

    442

---
+++ 442. Find All Duplicates in an Array/findallduplicatesinanarray.cs
@@ -0,0 +1,17 @@
+﻿public class Solution {
+    public IList<int> FindDuplicates(int[] nums) {
+        HashSet<int> dupList = new HashSet<int>();
+        HashSet<int> actualDups = new HashSet<int>();
+        
+        for(int i = 0; i < nums.Length; i++)
+        {
+            var exists = dupList.Add(nums[i]);   
+            
+            if(!exists)
+            {
+                actualDups.Add(nums[i]);
+            }
+        }
+        return actualDups.ToList();
+    }
+}
