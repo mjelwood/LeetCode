19c0915d9566491f18f0eea0030c5d4e87f01298
Michael Elwood <melwood@proformex.com>
2021-04-04 19:00:13 -0400

    sum of elements!

---
+++ 1748. Sum of Unique Elements/sumOfUniqueElements.cs
@@ -0,0 +1,29 @@
+﻿public class Solution {
+    public int SumOfUnique(int[] nums) {
+        int sum = 0;
+        Dictionary<int,int> countOfElements = new Dictionary<int,int>();
+        
+        for(int i = 0; i < nums.Length; i++)
+        {
+            int curKey = nums[i];
+            
+            if(countOfElements.ContainsKey(curKey))
+            {
+                countOfElements[curKey]++;
+            }
+            else
+            {
+                countOfElements.Add(curKey, 1);    
+            }
+            
+        }
+        
+        foreach(KeyValuePair<int,int> item in countOfElements)
+        {
+            if(item.Value == 1)
+                sum += item.Key;
+        }
+            
+        return sum;
+    }
+}
