d7ca6b4ca7a0df694eef695cfe0127faa0b1196d
Michael Elwood <melwood@proformex.com>
2021-04-04 20:10:21 -0400

    single number 2 with a twist of interface and class stuff

---
+++ 137. Single Number II - Refactored into Class with Interface/singleNumber.cs
@@ -0,0 +1,54 @@
+﻿public interface ISingleNumberHelper
+{
+    int SingleNumber(int[]nums );
+}
+
+public class SingleNumberHelper: ISingleNumberHelper
+{
+    public int SingleNumber(int[] nums) {
+     
+        Dictionary<int, int> currentCountOfKeys = new Dictionary<int, int>();
+        currentCountOfKeys = GetCountOfKeyValuesFromNumberArray(nums, currentCountOfKeys);
+        return returnSingleValue(currentCountOfKeys);
+    }
+
+    internal Dictionary<int, int> GetCountOfKeyValuesFromNumberArray(int[] nums, Dictionary<int,int> currentCountOfKeys)
+    {
+        for(int i = 0; i < nums.Length; i++)
+        {
+            int curKey = nums[i];
+            if(currentCountOfKeys.ContainsKey(curKey))
+            {
+                currentCountOfKeys[curKey]++;
+            }
+            else
+            {
+                currentCountOfKeys.Add(curKey, 1);    
+            }
+        }
+        
+        return currentCountOfKeys;
+    }
+    
+    internal int returnSingleValue(Dictionary<int,int> currentCountOfKeys)
+    {
+        int onlyAppearsOnce = 0;
+        foreach(KeyValuePair<int,int> item in currentCountOfKeys)
+        {
+            if(item.Value == 1)
+            {
+                onlyAppearsOnce = item.Key;
+                break;
+            }
+        }
+        return onlyAppearsOnce;
+    }
+}
+
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        SingleNumberHelper testHelper = new SingleNumberHelper();
+        
+        return testHelper.SingleNumber(nums);
+    }   
+}
