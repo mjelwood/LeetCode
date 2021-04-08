35f75c18c615bb4c7673c0860fa85aed26cf6270
Michael Elwood <melwood@proformex.com>
2021-04-07 21:45:47 -0400

    771 jewels and stones easy loop through and compare!

---
+++ 771. Jewels and Stones/jewelsandstones.cs
@@ -0,0 +1,16 @@
+﻿public class Solution {
+    public int NumJewelsInStones(string jewels, string stones) {
+        int countFound = 0;
+        
+        for(int j = 0; jewels.Length > j; j++)
+        {
+            for(int s = 0; stones.Length > s; s++)
+            {
+                if(jewels[j] == stones[s])
+                    countFound++;
+            }
+        }
+        
+        return countFound;
+    }
+}
